6362612dfe4996c688177bb68447c3373f45e496
Alexander Tsankov <antsankov@gmail.com>
2014-12-11 10:08:00 -0700

    Fixed drop script with new FK

--- drop_script.sql
+++ drop_script.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS `Book_Info`;
 DROP TABLE IF EXISTS `Class_Ratings`;
-DROP TABLE IF EXISTS `Major_Advisor_Info`;
 DROP TABLE IF EXISTS `Students`;
 DROP TABLE IF EXISTS `Professors`;
 DROP TABLE IF EXISTS `Student_School_Sponsor`;
 DROP TABLE IF EXISTS `Construction_Plans`;
 DROP TABLE IF EXISTS `Student_Status`;
+DROP TABLE IF EXISTS `Major_Advisor_Info`;
 DROP TABLE IF EXISTS `FCQs`;
 DROP TABLE IF EXISTS `Department_FCQs`;
 DROP TABLE IF EXISTS `Class_TA`;
